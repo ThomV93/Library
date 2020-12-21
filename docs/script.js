b1b4ac8489fcb47f823d0fe2a63fc5779fd8d354
Thomas Veit <thomas.veit93@gmail.com>
2020-12-21 19:40:14 +0000

    Create books and display in library

--- docs/script.js
+++ docs/script.js
@@ -1,8 +1,14 @@
+let myLibrary = [];
 const cardContainer_div = document.getElementById("card-container");
 const cardCreator_div = document.getElementById("creator-container");
 const plus_div = document.getElementById("plus");
 const formContainer_div = document.getElementById("form-container");
-const bookNum_span = document.getElementById("book-num");
+const bookTitle_input = document.getElementById("book-title");
+const author_input = document.getElementById("author");
+const pages_input = document.getElementById("total-pages");
+const formCancelBtn = document.getElementById("cancel-btn");
+const formCreateBtn = document.getElementById("create-btn");
+const bookCount_span = document.getElementById("book-num");
 const completedBookNum_span = document.getElementById("completed-book-num");
 const totalPageNum_span = document.getElementById("total-page-num");
 const totalCompletedPageNum_span = document.getElementById("total-completed-page-num");
@@ -27,89 +33,81 @@ function book(title, author, pages, read) {
 book.prototype = Object.create(library.prototype);
 
 //Create new book HTML structure for each book in the myLibrary array
-function displayBooks() {
-    myLibrary.forEach(item => {
-        //create card element
-        let bookCard = document.createElement("div");
-        bookCard.className = "card";
-    
-    
-        //card info section
-        let cardInfo = document.createElement("div");
-        cardInfo.className = "card-info";
-    
-        let cardTitle = document.createElement("h3");
-        cardTitle.className = "card-title";
-        cardTitle.innerHTML = item.title;
-    
-        let cardAuthor = document.createElement("p");
-        cardAuthor.className = "card-author";
-        cardAuthor.innerHTML = item.author;
-    
-        let cardPagesNum = document.createElement("p");
-        cardPagesNum.innerHTML = item.pages + " pages";
-    
-    
-        //switch checkbox
-        //outer section
-        let cardSwitchContainer = document.createElement("div");
-        cardSwitchContainer.className = "switch-container";
-    
-        let cardSwitchP = document.createElement("p");
-        cardSwitchP.innerHTML = "Read";
+function displayBooks(book) {
+    //create card element
+    let bookCard = document.createElement("div");
+    bookCard.className = "card";
 
-        //label section
-        let cardSwitchLabel = document.createElement("label");
-        cardSwitchLabel.className = "switch";
-        
-        //interior of label section
-        let cardSwitchLabelInput = document.createElement("input");
-        cardSwitchLabelInput.type = "checkbox";
-    
-        let cardSwitchLabelSpan = document.createElement("span");
-        cardSwitchLabelSpan.className = "slider round";
-        
-        //append info to label
-        cardSwitchLabel.append(cardSwitchLabelInput, cardSwitchLabelSpan);
-        //append all switch info to container
-        cardSwitchContainer.append(cardSwitchP, cardSwitchLabel);
 
-    
-        //Append all info inside the card info container
-        cardInfo.append(cardTitle, cardAuthor, cardPagesNum, cardSwitchContainer);
-        //End card info section
-    
+    //card info section
+    let cardInfo = document.createElement("div");
+    cardInfo.className = "card-info";
 
-        //Start card button section
-        let cardButtonContainer = document.createElement("div");
-        cardButtonContainer.className = "card-btn-container";
-        
-        let cardEditButton = document.createElement("button");
-        cardEditButton.id = "edit-btn";
-        cardEditButton.type = "button";
-        cardEditButton.innerHTML = "Edit";
+    let cardTitle = document.createElement("h3");
+    cardTitle.className = "card-title";
+    cardTitle.innerHTML = book.title;
+
+    let cardAuthor = document.createElement("p");
+    cardAuthor.className = "card-author";
+    cardAuthor.innerHTML = book.author;
+
+    let cardPagesNum = document.createElement("p");
+    cardPagesNum.innerHTML = book.pages + " pages";
+
+
+    //switch checkbox
+    //outer section
+    let cardSwitchContainer = document.createElement("div");
+    cardSwitchContainer.className = "switch-container";
+
+    let cardSwitchP = document.createElement("p");
+    cardSwitchP.innerHTML = "Read";
+
+    //label section
+    let cardSwitchLabel = document.createElement("label");
+    cardSwitchLabel.className = "switch";
     
-        let cardDeleteButton = document.createElement("button");
-        cardDeleteButton.id = "delete-btn";
-        cardEditButton.type = "button";
-        cardDeleteButton.innerHTML = "Delete"
+    //interior of label section
+    let cardSwitchLabelInput = document.createElement("input");
+    cardSwitchLabelInput.type = "checkbox";
 
-        //append info to button container
-        cardButtonContainer.append(cardEditButton, cardDeleteButton);
-        
-        //append card info to card container
-        bookCard.append(cardInfo, cardButtonContainer);
+    let cardSwitchLabelSpan = document.createElement("span");
+    cardSwitchLabelSpan.className = "slider round";
+    
+    //append info to label
+    cardSwitchLabel.append(cardSwitchLabelInput, cardSwitchLabelSpan);
+    //append all switch info to container
+    cardSwitchContainer.append(cardSwitchP, cardSwitchLabel);
 
-        //append card to card container div
-        cardContainer_div.appendChild(bookCard);
-    });
+
+    //Append all info inside the card info container
+    cardInfo.append(cardTitle, cardAuthor, cardPagesNum, cardSwitchContainer);
+    //End card info section
+
+
+    //Start card button section
+    let cardButtonContainer = document.createElement("div");
+    cardButtonContainer.className = "card-btn-container";
+    
+    let cardEditButton = document.createElement("button");
+    cardEditButton.id = "edit-btn";
+    cardEditButton.type = "button";
+    cardEditButton.innerHTML = "Edit";
+
+    let cardDeleteButton = document.createElement("button");
+    cardDeleteButton.id = "delete-btn";
+    cardEditButton.type = "button";
+    cardDeleteButton.innerHTML = "Delete"
+
+    //append info to button container
+    cardButtonContainer.append(cardEditButton, cardDeleteButton);
+    
+    //append card info to card container
+    bookCard.append(cardInfo, cardButtonContainer);
+
+    //append card to card container div
+    cardContainer_div.appendChild(bookCard);
 };
-
-//Display the user data in the side column
-function displayData() {
-    bookNum_span.innerHTML = myLibrary.length;
-};
-
 
 function addBookToLibrary(obj){
     myLibrary.push(obj);
@@ -117,21 +115,39 @@ function addBookToLibrary(obj){
 
 
 function openForm() {
-    plus_div.addEventListener("click", e => {
-        console.log(e);
+    plus_div.addEventListener("click", () => {
+        formContainer_div.style.display = "flex";
+    });
+};
+
+function closeForm() {
+    bookTitle_input.value = "";
+    author_input.value = "";
+    pages_input.value = "";
+    formContainer_div.style.display = "none";
+};
+
+function createBookForm(newBook) {
+    //cancel button
+    formCancelBtn.addEventListener("click", () => {
+        formContainer_div.style.display = "none";
+    });
+
+    //create button
+    formCreateBtn.addEventListener("click", () => {
+        let inputTitle = bookTitle_input.value;
+        let inputAuthor = author_input.value;
+        let inputPages = pages_input.value;
+
+        newBook = new book(inputTitle, inputAuthor, inputPages, "read");
+        addBookToLibrary(newBook);
+        displayBooks(newBook);
+        closeForm();
     });
 };
 
 
-
-const book1 = new book("Bend Sinister", "Nabakov", "450", "not read yet");
-const book2 = new book("1984", "Orwell", "350", "read");
-
-let myLibrary = [book1, book2];
-
-
 openForm();
-displayBooks();
-displayData();
+createBookForm();
 
   
